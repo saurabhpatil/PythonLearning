a4c48b62c10f50e7cfe7765f931367743a8de622
saurabhpatil8989 <saurabhpatil2007@gmail.com>
2015-05-29 15:18:57 +0530

    creating a working renaming file

--- files.py
+++ files.py
@@ -1,16 +1,36 @@
-import os, time, re
+import os, time, re, locale
 from operator import itemgetter
+from functools import cmp_to_key
 
-path = r"C:\Users\saurabh\Documents"
-	
-PS_course = open(os.path.join(path, 'filelist.txt'))
+file_count = 0
+path = r"C:\Users\saurabh\Desktop\Big Data - The Big Picture"
+os.chdir(path)
+
+PS_course = open('filelist.txt')
 PS_courselist = ('').join(PS_course.readlines())
-PS_files = re.findall('[a-z][a-z: ]+', PS_courselist, re.I | re.M | re.X)
-PS_folders = re.findall('(^(?!\s)[a-z][a-z ]*)', PS_courselist, re.I | re.M | re.X)
+PS_files = re.findall('[a-z][a-z: \d]+', PS_courselist, re.I | re.M | re.X)
+PS_folders = re.findall('(^(?!\s)[a-z][a-z \d]*)', PS_courselist, re.I | re.M | re.X)
 
-for root, dirs, files in os.walk(path):
-	for dir in dirs:
+print(PS_folders)
+for root, dirs, files in os.walk("."):
+	for dir_idx, dir in enumerate(dirs):
+		print(dir_idx , dir, PS_folders[dir_idx])
+		new_dir_name = str(dir_idx+1)+". "+PS_folders[dir_idx] 
+		os.rename(dir, new_dir_name)
 		
+		directory_path = os.path.join(path,new_dir_name)
+		file_list = list()
+		for files in os.listdir(directory_path):
+			file_path = os.path.join(directory_path,files)
+			file_list.append((files,time.ctime(os.stat(file_path).st_ctime)))
+		sorted_list = sorted(file_list, key=itemgetter(1))
+		for idx, item in enumerate(sorted_list):
+			file_name = os.path.join(directory_path, item[0])
+			os.rename(file_name, os.path.join(directory_path,
+						   str(idx+1)+". "+PS_files[file_count]))
+			file_count+=1
+		print('-'*50)
+		print('File renaming completed: '+new_dir_name)
 	break
 
 # for directories in os.listdir(path):
@@ -28,5 +48,3 @@ for root, dirs, files in os.walk(path):
 		# count+=1
 	# print('-'*50)
 	# print('File renaming completed: '+directories)
-
-# print(PS_files)
