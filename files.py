fb9f58585f4f8d9eecda9701a1ea124734308a09
saurabhpatil8989 <saurabhpatil2007@gmail.com>
2015-06-02 22:12:55 +0530

    Renaming python file development completed

--- files.py
+++ files.py
@@ -2,19 +2,19 @@ import os, time, re ,sys
 from operator import itemgetter
 
 file_count = 1
-path = r"C:\Users\spatil\Desktop\PluralSight\Understanding .NET and WCF Transactions"
+path = sys.argv[1]
 os.chdir(path)
 
 PS_course = open('filelist.txt')
 PS_courselist = ('').join(PS_course.readlines())
-PS_files = re.findall('[a-z.][a-z: \d,]+', PS_courselist, re.I | re.M | re.X)
+PS_files = re.findall('[a-z.][a-z: \d,/?]+', PS_courselist, re.I | re.M | re.X)
 PS_folders = re.findall('(^(?!\s)[a-z.][a-z \d,]*)', PS_courselist, re.I | re.M | re.X)
 
 for root, dirs, files in os.walk("."):
 	for dir_idx, dir in enumerate(dirs):
 		new_dir_name = str(dir_idx+1)+". "+PS_folders[dir_idx] 
 		print(dir+' --> '+new_dir_name+"\n")
-		os.rename(dir, new_dir_name)
+		#os.rename(dir, new_dir_name)
 		
 		directory_path = os.path.join(path,new_dir_name)
 		file_list = list()
@@ -26,8 +26,8 @@ for root, dirs, files in os.walk("."):
 			file_name = os.path.join(directory_path, item[0])
 			new_file_name = PS_files[file_count].replace(':', ' -').replace('/', '&')+".mp4"
 			print(item[0]+' --> '+(str(idx+1)+". "+new_file_name))
-			os.rename(file_name, os.path.join(directory_path,
-						   str(idx+1)+". "+new_file_name))
+			# os.rename(file_name, os.path.join(directory_path,
+						   # str(idx+1)+". "+new_file_name))
 			file_count+=1
 		print("\n"+'File renaming completed: '+new_dir_name)
 		print('-'*50)
